c91d3c90d5e63c5023bdc98c47f08c33b9863fb5
raghdaa-gad <raghdaa.gad7@gmail.com>
2026-09-24 16:29:13 +0300

    update

--- AcademyScheduleAnalyzer/Program.cs
+++ AcademyScheduleAnalyzer/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.Globalization;
+
+
+/*Part 1 — Starter Data*/
 string[] sessionNames =
 {
     "C# Basics",
@@ -26,15 +29,62 @@ int[] sessionDurations =
     180
 };
 
+
+/*Part 2 — Display All Sessions*/
 static void DisplayAllSessions(string[] sessionNames, DateTime[] sessionDates, int[] sessionDurations)
 {
-
     for (int i = 0; i < sessionNames.Length; i++)
     {
         Console.WriteLine($"{i + 1}. {sessionNames[i]}");
-        Console.WriteLine($"Date: {sessionDates[i]:dd MMMM yyyy}");
-        Console.WriteLine($"Start Time : {sessionDates[i]:hh:mm tt}");
+        Console.WriteLine($"Date: {sessionDates[i].ToString("dd MMMM yyyy", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"Start Time: {sessionDates[i].ToString("hh:mm tt", CultureInfo.InvariantCulture)}");
         Console.WriteLine($"Duration: {sessionDurations[i]} minutes");
     }
 }
-DisplayAllSessions(sessionNames, sessionDates, sessionDurations);
+
+//DisplayAllSessions(sessionNames, sessionDates, sessionDurations);
+
+
+/*Part 3 — Search for a Session*/
+static void SearchSession(string[] sessionNames, DateTime[] sessionDates, int[] sessionDurations, string searchName)
+{
+    int index = Array.FindIndex(sessionNames, name => name == searchName);
+    if (index != -1)
+    {
+        Console.WriteLine($"{index + 1}. {sessionNames[index]}");
+        Console.WriteLine($"Date: {sessionDates[index].ToString("dd MMMM yyyy", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"Start Time: {sessionDates[index].ToString("hh:mm tt", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"Duration: {sessionDurations[index]} minutes");}
+      else
+       Console.WriteLine("Session not found.");
+}
+//SearchSession(sessionNames, sessionDates, sessionDurations,"C# Basics");
+
+
+/*Part 4 — Array Methods Practice*/
+//4.1 Sort Session Names
+static void SortSessionNames(string[] sessionNames)
+{
+    
+    string[] sortSession = new string[sessionNames.Length];
+    Array.Copy(sessionNames,sortSession,sessionNames.Length);
+    Array.Sort(sortSession);
+    foreach (var item in sortSession)
+    {
+        Console.WriteLine(item);
+    }
+
+}
+//SortSessionNames(sessionNames);
+
+
+//4.2 Reverse Session Names
+static void ReverseSessionNames(string[] sessionNames)
+{
+    string[] reverseSession = new string[sessionNames.Length];
+    Array.Copy(sessionNames,reverseSession,sessionNames.Length);
+    Array.Reverse(reverseSession);
+    foreach(var item in reverseSession)
+        Console.WriteLine(item);
+}
+ReverseSessionNames(sessionNames);
